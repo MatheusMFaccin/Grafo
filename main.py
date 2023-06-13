2362820f7e4a3567107972e67f340f7723e7a1de
Matheus Machado Faccin <96211501+MatheusMFaccin@users.noreply.github.com>
2023-06-12 22:08:08 -0300

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pyvis import Network
+from pyvis.network import Network
 import networkx as nx
 
 class Vertice:
@@ -6,26 +6,30 @@ class Vertice:
         self.index = index
 
 class Aresta:
-    def __init__(self,inicial,fim,custo,carga):
+    def __init__(self,inicial,fim,custo,carga,eficiencia):
         self.inicial = inicial
         self.fim = fim
         self.custo = custo
         self.carga = carga
+        self.eficiencia = eficiencia
 class Grafo:
     def __init__(self, arestas):
         self.arestas = arestas
 
-
+net = Network(notebook=True,directed=True,height='500px', width='100%')
+nxgraph = nx.cycle_graph(10)
 f = open("arquivo.txt", "r")
 Arquivo = f.read()
 linhas = Arquivo.split("\n")
 Vertices = []
 Arestas = []
+eficiencias = []
 for linha in range(len(linhas)):
     if linha == 0:
         vertices = linhas[0].split(',')
         for vertice in vertices:
-            Vertices.append(Vertice(vertice))
+            Vertices.append(Vertice(int(vertice)))
+            
     else:
         arestas = linhas[linha].split(';')
         print(arestas)
@@ -33,20 +37,40 @@ for linha in range(len(linhas)):
             if aresta:
                 valores = aresta.split(',')
                 if valores:
-                    verticeInicial = valores[0]
-                    verticeFinal = valores[1]
-                    custo = valores[2]
-                    carga = valores[3]
+                    verticeInicial = int(valores[0])
+                    verticeFinal = int(valores[1])
+                    custo = float(valores[2])
+                    carga = float(valores[3])
+                    eficiencia = carga/custo
                     if any(vertice.index == verticeFinal for vertice in Vertices) and any(vertice.index==verticeInicial for vertice in Vertices): 
-                        Arestas.append(Aresta(verticeInicial,verticeFinal,custo,carga))
-net = Network()
+                        Arestas.append(Aresta(verticeInicial,verticeFinal,custo,carga,eficiencia))
 
-for aresta in Arestas:
-    print(aresta.inicial,aresta.fim,aresta.custo,aresta.carga)
-    net.add_edge(aresta.inicial,aresta.fim, custo = aresta.custo, carga = aresta.carga)
 
-custos = nx.get_edge_attributes(net, 'custo')
-cargas = nx.get_edge_attributes(net, 'carga')
+for vertice in Vertices:
+    print(vertice.index)
+   
+    net.add_node(int(vertice.index), label="vertice "+str(vertice.index),shape="circle",color="#fa8072")
+arestasOrdenadas = sorted(Arestas, key=lambda  obj: -obj.eficiencia)
+i = 0
+for aresta in arestasOrdenadas:
+    if i == 0:    
+        print(aresta.inicial,aresta.fim,aresta.custo,aresta.carga)
+        resultado = round(aresta.custo/aresta.carga,2)
+        eficiencias.append(resultado)
+        net.add_edge(int(aresta.inicial),int(aresta.fim),color="#0000ff",weight=5,title="custo da rota: "+str(aresta.custo)+" tamanho da carga: "+str(aresta.carga)+" eficiencia da rota: "+str(resultado))
+    else:
+        print(aresta.inicial,aresta.fim,aresta.custo,aresta.carga)
+        resultado = round(aresta.custo/aresta.carga,2)
+        eficiencias.append(resultado)
+        net.add_edge(int(aresta.inicial),int(aresta.fim),weight=5,title="custo da rota: "+str(aresta.custo)+" tamanho da carga: "+str(aresta.carga)+" eficiencia da rota: "+str(resultado))
+    i+=1
+
+
+for aresta in arestasOrdenadas:    
+    print(aresta.inicial,aresta.fim,aresta.custo,aresta.carga, aresta.eficiencia)
+
+
+net.show('index.html')
     
     
 
